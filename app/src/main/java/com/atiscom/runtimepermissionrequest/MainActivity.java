f2128ee8c192bd77c3f5f11e1d282e9c7c064118
tldn1 <muamer.beginovic@outlook.com>
2017-11-10 14:13:21 +0100

    Response permission handled.

--- app/src/main/java/com/atiscom/runtimepermissionrequest/MainActivity.java
+++ app/src/main/java/com/atiscom/runtimepermissionrequest/MainActivity.java
@@ -3,6 +3,7 @@ package com.atiscom.runtimepermissionrequest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionInfo;
+import android.hardware.Camera;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,6 +11,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.jar.Manifest;
 
@@ -103,6 +105,50 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    @Override
 
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERM_REQ) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+
+            }
+
+        }else if(requestCode == STORAGE_PERM_REQ){
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Storage permission granted", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(this, "Storage permission denied", Toast.LENGTH_LONG).show();
+
+            }
+
+        }else if (requestCode == CONTACTS_PERM_REQ){
+
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Read contacts  permission granted", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(this, "Read contacts permission denied", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+    }
 
 }
